8c222fd8640fe80a14d6e310fc15727126b22b48
momchilmitev <momchilmitev87@gmail.com>
2020-11-09 18:53:28 +0100

    basic DRY refactoring

--- app.js
+++ app.js
@@ -1,52 +1,65 @@
 "use strict";
 
+// DOM elements
+let $message = document.querySelector(".message");
+let $highscore = document.querySelector(".highscore");
+let $score = document.querySelector(".score");
+let $number = document.querySelector(".number");
+let $guess = document.querySelector(".guess");
+let $tryBtn = document.querySelector(".try");
+let $resetBtn = document.querySelector(".again");
+
+// Other variables
 let randomNumber = generateRandomNumber();
 let score = 20;
 let highscore = 0;
 
-document.querySelector(".try").addEventListener("click", function (e) {
-  let guessingNumber = Number(document.querySelector(".guess").value);
-  document.querySelector(".number").textContent = guessingNumber;
+// Start the game
+$tryBtn.addEventListener("click", function (e) {
+  let guessingNumber = Number($guess.value);
+  setValueToDomElement($number, guessingNumber);
 
   if (!guessingNumber) {
-    document.querySelector(".message").textContent = "No number provided!";
+    setValueToDomElement($message, "No number provided!");
   } else if (guessingNumber === randomNumber) {
-    document.querySelector(".message").textContent = "Correct!!!";
+    setValueToDomElement($message, "Correct!!!");
     if (score > highscore) {
       highscore = score;
-      document.querySelector(".highscore").textContent =
-        "Highscore: " + highscore;
+      setValueToDomElement($highscore, "Highscore: " + highscore);
     }
-  } else if (guessingNumber < randomNumber) {
+  } else if (guessingNumber !== randomNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too low!";
+      setValueToDomElement(
+        $message,
+        guessingNumber > randomNumber ? "Too High!" : "Too Low!"
+      );
       score--;
-      document.querySelector(".score").textContent = "Score: " + score;
+      setValueToDomElement($score, "Score: " + score);
     } else {
-      document.querySelector(".message").textContent = "Game over!";
-      document.querySelector(".score").textContent = "Score: " + 0;
-    }
-  } else if (guessingNumber > randomNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too high!";
-      score--;
-      document.querySelector(".score").textContent = "Score: " + score;
-    } else {
-      document.querySelector(".score").textContent = "Score: " + 0;
-      document.querySelector(".message").textContent = "Game over!";
+      setValueToDomElement($message, "Game over!");
+      setValueToDomElement($score, "Score: " + 0);
     }
   }
 });
 
-document.querySelector(".again").addEventListener("click", function (e) {
+// Reset the game
+$resetBtn.addEventListener("click", function (e) {
   randomNumber = generateRandomNumber();
   score = 20;
-  document.querySelector(".score").textContent = "Score: " + score;
-  document.querySelector(".message").textContent = "Let's try!";
-  document.querySelector(".number").textContent = "Your number!";
-  document.querySelector(".guess").value = "";
+  setValueToDomElement($score, "Score: " + score);
+  setValueToDomElement($message, "Let's play!");
+  setValueToDomElement($number, "Your number!");
+  setValueToDomElement($guess, "", true);
 });
 
+// Helper functions
 function generateRandomNumber() {
   return Math.trunc(Math.random() * 20) + 1;
+}
+
+function setValueToDomElement(element, elValue, isInput = false) {
+  if (isInput) {
+    element.value = elValue;
+  }
+  element.textContent = elValue;
 }
